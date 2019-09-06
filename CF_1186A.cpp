cc0d70f7c399e41091b26a94ced5c4629f183c58
TAFIMnoor <43815369+TAFIMnoor@users.noreply.github.com>
2019-09-06 16:50:13 +0600

    Create CF_1186A.cpp

---
+++ CF_1186A.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int n,s,e;
+    cin>>n>>s>>e;
+    if(n<=s && n<=e)
+        cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+    return 0;
+}
